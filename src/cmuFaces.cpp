040efce137983d31713ec00c4f68cc63ac74a413
Jacob Emmel <jdemmel94@comcast.net>
2017-02-23 10:14:39 -0700

    CMU Demo: run face detector on images
    Expanded `process` function to run the detector and display the results.

--- src/cmuFaces.cpp
+++ src/cmuFaces.cpp
@@ -1,5 +1,5 @@
 /* ***
- * WIP: Iterates through the CMU Face Images data set.
+ * Iterates through the CMU Face Images data set and runs OpenCV HAAR Cascade detectors on the images.
  *
  * File names are formatted [User ID]/[User ID]_[Pose]_[Expression]_[Eyes]_[Scale].pgm
  * _[Scale] is omitted for scale=1
@@ -8,7 +8,7 @@
  *   https://archive.ics.uci.edu/ml/datasets/CMU+Face+Images
  *   https://archive.ics.uci.edu/ml/machine-learning-databases/faces-mld/faces.data.html
  *
- * Licensing:
+ * Data Set Licensing:
  *   Usable for any educational purpose with attribution
  * ***/
 
@@ -16,23 +16,29 @@
 #include <iostream>
 
 #include <opencv2/opencv.hpp>
+#include "SimpleFaceDetector.h"
 
 using std::cout;
 using std::cerr;
 using std::endl;
 using std::string;
 
-bool process(const string dir, const string userId, const string pose, const string expression, const string eye, const int scale) {
-    string scaleStr = scale == 0 ? "" : ("_" + std::to_string(scale));
-    string imgFilePath = dir + userId + "/" + userId + "_" + pose + "_" + expression + "_" + eye + scaleStr + ".pgm";
+bool process(SimpleFaceDetector& faceDetector, const string dir, const string userId, const string pose,
+             const string expression, const string eye, const int scale) {
+    cv::Mat imageOverlaid;
 
-    cv::Mat image = cv::imread(imgFilePath, cv::IMREAD_UNCHANGED);
+    const string scaleStr = scale == 0 ? "" : ("_" + std::to_string(scale));
+    const string imgFilePath = dir + userId + "/" + userId + "_" + pose + "_" + expression + "_" + eye + scaleStr + ".pgm";
+
+    cv::Mat image = cv::imread(imgFilePath, cv::IMREAD_COLOR);
     if (image.data == NULL) {
         cerr << "Could not load image " << imgFilePath << " (may be bad?)" << endl;
         return false;
     }
 
     cout << "Loaded " << imgFilePath << endl;
+    imageOverlaid = faceDetector.detectAndOverlay(image);
+    cv::imshow("Face Detections", imageOverlaid);
     return true;
 }
 
@@ -42,6 +48,9 @@ int main(int argc, char** argv) {
         cerr << "PATH: Path to the CMU data set (with ending /)" << endl;
         return EXIT_FAILURE;
     }
+
+    const std::string faceCascadePath = "haarcascade_frontalface_alt.xml";
+    const std::string eyesCascadePath = "haarcascade_eye_tree_eyeglasses.xml";
 
     // User IDs identify the twenty people whose images comprise the data set
     const std::vector<string> userIds = {
@@ -54,23 +63,39 @@ int main(int argc, char** argv) {
     const std::vector<int> scales = { 0, 2, 4 };
 
     cv::Mat image;
-    string imgFilePath;
-    string scaleStr;
+    cv::CascadeClassifier faceCascade;
+    cv::CascadeClassifier eyesCascade;
+
     string imgDir;
+    string imgFilePath;
 
     imgDir = argv[1];
 
+    if(!faceCascade.load(cv::String(faceCascadePath))) {
+        cerr << "Error loading face cascade .xml file." << endl;
+        return EXIT_FAILURE;
+    };
+
+    if(!eyesCascade.load(cv::String(eyesCascadePath))) {
+        cerr << "Error loading eye cascade .xml file." << endl;
+        return EXIT_FAILURE;
+    };
+
+    // Loop through the data set, running valid images through a face detector
+    SimpleFaceDetector detector(faceCascade, eyesCascade);
     for (const string& userId : userIds) {
         for (const string& pose : poses) {
             for (const string& expression : expressions) {
                 for (const string& eye : eyes) {
                     for (const int& scale : scales) {
-                        process(imgDir, userId, pose, expression, eye, scale);
+                        process(detector, imgDir, userId, pose, expression, eye, scale);
+                        cv::waitKey(300);
                     }
                 }
             }
         }
     }
 
+    cv::destroyWindow("Face Detections");
     return EXIT_SUCCESS;
 }
