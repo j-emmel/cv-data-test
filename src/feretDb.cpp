81fbbf9083a7cee0f085edfc597ebe9262572595
Jacob Emmel <jdemmel94@comcast.net>
2017-03-01 22:33:00 -0700

    FERET demo iterates through data set
    The color FERET demo now reads through the images in the data set, rather than opening one image file. The images read from the set are displayed to the user, but not otherwise processed.

--- src/feretDb.cpp
+++ src/feretDb.cpp
@@ -30,38 +30,78 @@ using std::cerr;
 using std::endl;
 using std::string;
 
+/**
+ * Reads a Bzip2-compressed image file into an OpenCV matrix.
+ * @param imgFilePath path to the compressed image file
+ * @return A matrix containing the image data, or an empty matrix if the image could not be read
+ */
+cv::Mat imreadBz2(const string imgFilePath) {
+    boost::iostreams::file_source bz2Input(imgFilePath, std::ios_base::in | std::ios_base::binary);
+
+    if (!bz2Input.is_open()) {
+        cout << "Invalid input: " << imgFilePath << endl;
+        return cv::Mat();
+    }
+
+    // Set up a filtering stream to unzip the file
+    boost::iostreams::filtering_istream bunzip2Filter;
+    bunzip2Filter.push(boost::iostreams::bzip2_decompressor());
+    bunzip2Filter.push(bz2Input);
+
+    // Convert the stream to an OpenCV Mat
+    std::vector<char> tmp;
+    boost::iostreams::copy(bunzip2Filter, boost::iostreams::back_inserter(tmp));
+    return cv::imdecode(tmp, cv::IMREAD_COLOR);
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         cerr << "Usage: ./feret_demo PATH" << endl;
-        cerr << "PATH: Path to a bz2-compressed image file." << endl;
-        //cerr << "PATH: Path to the COLORFERET directory (with ending /)" << endl;
+        cerr << "PATH: Path to color FERET directory, with final \"/\"." << endl;
         return EXIT_FAILURE;
     }
 
     string imgFilePath = argv[1];
-    boost::filesystem::path imgPath(imgFilePath);
 
-    if (boost::filesystem::exists(imgPath) && boost::filesystem::extension(imgPath) == ".bz2") {
-        boost::iostreams::file_source bz2Input(imgFilePath, std::ios_base::in | std::ios_base::binary);
+    try {
+        for (int i = 1; i <= 1208; i++) {
+            int dvdNum = (i < 740) ? 1 : 2;
 
-        // Set up a filtering stream to unzip the file
-        boost::iostreams::filtering_istream bunzip2Filter;
-        bunzip2Filter.push(boost::iostreams::bzip2_decompressor());
-        bunzip2Filter.push(bz2Input);
+            std::ostringstream subjectStream;
+            subjectStream << imgFilePath << "dvd" << dvdNum << "/data/images/"
+                          << std::setw(5) << std::setfill('0') << i;
+            boost::filesystem::path subjectDir(subjectStream.str());
 
-        // Convert the stream to an OpenCV Mat
-        std::vector<char> tmp;
-        boost::iostreams::copy(bunzip2Filter, boost::iostreams::back_inserter(tmp));
-        cv::Mat image = cv::imdecode(tmp, cv::IMREAD_GRAYSCALE);
+            if (!boost::filesystem::exists(subjectDir)) {
+                cout << "Could not find subject " << i << " directory " << subjectDir << endl;
+                continue;
+            }
 
-        // Display image for three seconds
-        cout << "Matrix size: " << image.size() << endl;
-        cv::imshow("Extracted Image", image);
-        cv::waitKey(3000);
-        cv::destroyWindow("Extracted Image");
-    } else {
-        cout << "Invalid input: " << imgFilePath << endl;
+            if (boost::filesystem::is_regular_file(subjectDir)) {
+                cout << "Ignoring file (expected directory) " << subjectDir << endl;
+                continue;
+            } else if (boost::filesystem::is_directory(subjectDir)) {
+                for (boost::filesystem::directory_entry entry :
+                        boost::make_iterator_range(boost::filesystem::directory_iterator(subjectDir))) {
+
+                    if (boost::filesystem::extension(entry.path()) == ".bz2") {
+                        cv::Mat image = imreadBz2(entry.path().string());
+                        if (!image.empty()) {
+                            //cout << "Read in " << entry.path() << endl;
+                            cv::imshow("Extracted Image", image);
+                            cv::waitKey(300);
+                        }
+                    }
+                }
+            } else {
+                cout << subjectDir << " exists, but is neither a regular file nor a directory" << endl;
+            }
+        }
+    } catch (const boost::filesystem::filesystem_error& ex) {
+        cout << ex.what() << '\n';
     }
+
+    cv::destroyWindow("Extracted Image");
 
     return EXIT_SUCCESS;
 }
