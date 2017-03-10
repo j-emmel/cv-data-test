d17aae6a23478f54539f9cb471646ce43eed5298
Jacob Emmel <jdemmel94@comcast.net>
2017-03-10 15:08:39 -0700

    Prepare FERET filter script to support filtering
    Added a filter class that is referenced to decide if a given subject or image should be included in the output.
    
    Output is now only printed to console.

--- scripts/feret_filter.rb
+++ scripts/feret_filter.rb
@@ -23,10 +23,10 @@ class FeretSubject
   end
 
   def self.from_xml(subject_xml)
-    FeretSubject.new(subject_xml.at_xpath('@id'),
-                     subject_xml.at_xpath('//Gender/@value'),
-                     subject_xml.at_xpath('//YOB/@value'),
-                     subject_xml.at_xpath('//Race/@value'))
+    FeretSubject.new(subject_xml.at_xpath('@id').to_s,
+                     subject_xml.at_xpath('//Gender/@value').to_s,
+                     subject_xml.at_xpath('//YOB/@value').to_s,
+                     subject_xml.at_xpath('//Race/@value').to_s)
   end
 end
 
@@ -49,9 +49,9 @@ class ImageData
   def self.from_xml(subject_xml, img_path)
     face_xml = subject_xml.at_xpath('//Application//Face')
     hair_xml = face_xml.at_xpath('//Hair')
-    ImageData.new(subject_xml.at_xpath('@id'),
-                  img_path,
-                  self.eql_ignore_case?('Yes', face_xml.at_xpath('//Pose/@name')),
+    ImageData.new(subject_xml.at_xpath('@id').to_s,
+                  img_path.to_s,
+                  face_xml.at_xpath('//Pose/@name').to_s,
                   self.eql_ignore_case?('Yes', face_xml.at_xpath('//Wearing/@glasses')),
                   self.eql_ignore_case?('Yes', hair_xml.at_xpath('@beard')),
                   self.eql_ignore_case?('Yes', hair_xml.at_xpath('@mustache')))
@@ -59,6 +59,53 @@ class ImageData
 
   def self.eql_ignore_case?(string1, string2)
     string1.casecmp(string2) == 0
+  end
+end
+
+class SubjectFilters
+  attr_writer :genders, :poses
+
+  def initialize
+    @inclusions = %i(any yes no)
+    @genders = %w(Male Female)
+    @poses = %w(fa fb pl hl ql pr hr qr ra rb rc rd re)
+    @glasses = :any
+    @beard = :any
+    @mustache = :any
+  end
+
+  def to_s
+    "{ SubjectFilters | genders: #{@genders}, poses: #{@poses}, glasses: #{@glasses}, beard: #{@beard}, mustache: #{@mustache} }"
+  end
+
+  def glasses=(glasses)
+    return unless @inclusions.include?(glasses.to_sym)
+    @glasses = glasses.to_sym
+  end
+
+  def mustache=(mustache)
+    return unless @inclusions.include?(mustache.to_sym)
+    @mustache = mustache.to_sym
+  end
+
+  def beard=(beard)
+    return unless @inclusions.include?(beard.to_sym)
+    @beard = beard.to_sym
+  end
+
+  def self.do_accept?(inclusion, value)
+    (inclusion == :any) || (inclusion == :no && value == false) || (inclusion == :yes && value == true)
+  end
+
+  def accept_subject?(subject)
+    @genders.include?(subject.gender)
+  end
+
+  def accept_image?(image_data)
+    (@poses.include?(image_data.pose) &&
+        self.class.do_accept?(@glasses, image_data.glasses) &&
+        self.class.do_accept?(@beard, image_data.beard) &&
+        self.class.do_accept?(@mustache, image_data.mustache))
   end
 end
 
@@ -85,15 +132,18 @@ def get_image_data_from_file(xml_file_path)
   ImageData.from_xml(recording.at_xpath('//Subject'), img_path)
 end
 
-def process_dvd(feret_path, dvd_num)
+def process_dvd(feret_path, dvd_num, filters)
   feret_xml_path = "#{feret_path}/dvd#{dvd_num}/data/ground_truths/xml"
 
   # Iterate over the directories for each subject
   Dir.foreach(feret_xml_path) do |subject_xml_dir|
     # Ignore current and parent directories
-    next if subject_xml_dir == '.' || subject_xml_dir == '..'
+    next if subject_xml_dir == '.' || subject_xml_dir == '..' || !Dir.exist?("#{feret_xml_path}/#{subject_xml_dir}")
 
-    puts get_demographics_from_file(feret_xml_path, subject_xml_dir)
+    # Filter out this image subject based on demographic information
+    feret_subject = get_demographics_from_file(feret_xml_path, subject_xml_dir)
+    next unless filters.accept_subject?(feret_subject)
+    puts feret_subject
 
     # Iterate over the files in a single subject's directory
     subject_xml_full_path = "#{feret_xml_path}/#{subject_xml_dir}"
@@ -102,7 +152,11 @@ def process_dvd(feret_path, dvd_num)
       next if (File.extname(xml_file_name) != '.xml') || (File.basename(xml_file_name, '.xml') == subject_xml_dir)
 
       xml_file_path = "#{subject_xml_full_path}/#{xml_file_name}"
-      puts get_image_data_from_file(xml_file_path)
+      feret_image_data = get_image_data_from_file(xml_file_path)
+      next unless filters.accept_image?(feret_image_data)
+
+      #TODO: record accepted image
+      puts feret_image_data
     end
   end
 end
@@ -126,5 +180,8 @@ unless Dir.exist?(feret_path)
   exit -1
 end
 
-process_dvd(feret_path, 1)
+filters = SubjectFilters.new
+#TODO: configure filters
+
+process_dvd(feret_path, 1, filters)
 #process_dvd(feret_path, 2)
